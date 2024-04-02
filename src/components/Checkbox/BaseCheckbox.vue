6edd51267a394a5cfc615929ddc57b778f3bd3d1
hyeon9782 <hyeon9782@gmail.com>
2024-04-02 22:31:19 +0900

    feat: BaseCheckbox 컴포넌트 생성

---
+++ src/components/Checkbox/BaseCheckbox.vue
@@ -0,0 +1,10 @@
+<template>
+  <label>
+    <input type="checkbox" />
+    <span>
+      <slot></slot>
+    </span>
+  </label>
+</template>
+<script lang="ts" setup></script>
+<style lang=""></style>
