c978d644eb2fa23e180dc4496fcbdf0d12fbb10a
hyeon9782 <hyeon9782@gmail.com>
2024-04-04 00:07:42 +0900

    feat: List 관련 컴포넌트 생성

---
+++ src/components/List/BaseItem.vue
@@ -0,0 +1,7 @@
+<template>
+  <li>
+    <slot></slot>
+  </li>
+</template>
+<script lang="ts" setup></script>
+<style lang=""></style>
---
+++ src/components/List/BaseList.vue
@@ -0,0 +1,7 @@
+<template>
+  <ul>
+    <slot></slot>
+  </ul>
+</template>
+<script lang="ts" setup></script>
+<style lang=""></style>
